f94c4a5bfed6c746d64f3e27cb06545ece3f4cf2
ww <1074466590@qq.com>
2024-11-16 15:08:18 +0800

    Version 1.0.0 Implementation of a simple INotifierChange interface and command implementation (with or without parameters)

---
+++ MyToolKit.MVVM/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+
+namespace MyToolKit.MVVM;
+
+public class RelayCommand<T> : ICommand
+{
+    private Action<T> _execute;
+    
+    public RelayCommand(Action<T> execute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+    }
+    
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute((T)parameter!);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
+
+public class RelayCommand : ICommand
+{
+    private Action<object> _execute;
+    
+    public RelayCommand(Action<object> execute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+    }
+    
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(parameter!);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
