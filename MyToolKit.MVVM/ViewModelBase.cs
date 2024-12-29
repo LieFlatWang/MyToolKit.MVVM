16ba508278f66a8ad3288aa9d71fd063c1003ac4
ww <1074466590@qq.com>
2024-12-29 14:33:38 +0800

    Add ViewModelBase And  instructions for use

---
+++ MyToolKit.MVVM/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿namespace MyToolKit.MVVM;
+
+public class ViewModelBase : ObservableObject
+{
+    private static ViewModelBase? _instance;
+    private static readonly object Lock = new object();
+
+    public static ViewModelBase Instance
+    {
+        get
+        {
+            // First check outside of the lock
+            if (_instance == null)
+            {
+                lock (Lock)
+                {
+                    // Second check inside the lock to ensure only one instance is created
+                    if (_instance == null)
+                    {
+                        _instance = new ViewModelBase();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
